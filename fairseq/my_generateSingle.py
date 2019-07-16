e405cab1db97ae7ad3f2ab7f2f59d178637c0a3a
Xing <s1879281@hotmail.com>
2019-07-16 15:57:29 +0100

    gen

--- fairseq/my_generateSingle.py
+++ fairseq/my_generateSingle.py
@@ -196,7 +196,7 @@ if __name__ == '__main__':
                    help='path to save predictions')
     group.add_argument('--reference-dir', metavar='DIR', default='valid',
                    help='path to save predictions')
-    parser.add_argument('--outindices', required=False,type=str,
+    parser.add_argument('--outindices', default='excluded_indices/valid_ignoredIndices.log', required=False,type=str,
                             help='load set of indices that were out for a category dataset.')
 
     args = options.parse_args_and_arch(parser)
