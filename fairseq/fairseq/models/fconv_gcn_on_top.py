16c5523880d6a24ecf40ab03355c1364ccd44e10
Xing <s1879281@hotmail.com>
2019-07-16 23:35:30 +0100

    fconvgcnontop

--- fairseq/fairseq/models/fconv_gcn_on_top.py
+++ fairseq/fairseq/models/fconv_gcn_on_top.py
@@ -21,8 +21,8 @@ from . import (
 from .fconv import FConvDecoder, extend_conv_spec, Embedding, PositionalEmbedding, Linear, ConvTBC
 
 
-@register_model('fconv_gcn')
-class FConvGCNModel(FairseqGCNModel):
+@register_model('fconv_gcn_on_top')
+class FConvGCNOnTopModel(FairseqGCNModel):
     def __init__(self, encoder, decoder):
         super().__init__(encoder, decoder)
         self.encoder.conv_encoder.num_attention_layers = sum(layer is not None for layer in decoder.attention)
@@ -93,11 +93,17 @@ class FConvGCNModel(FairseqGCNModel):
             decoder_embed_dict = utils.parse_embedding(args.decoder_embed_path)
             utils.print_embed_overlap(decoder_embed_dict, task.target_dictionary)
 
-        gcn_encoder = GCNEncoder(
+        conv_encoder = FConvEncoder3(
+            dictionary=task.source_dictionary,
+            embed_dim=args.encoder_embed_dim,
+            embed_dict=encoder_embed_dict,
+            convolutions=eval(args.encoder_layers),
+            dropout=args.dropout,
+            max_positions=args.max_source_positions,
+            normalization_constant=args.normalization_constant,
+        )
+        gcn_encoder = GCNEncoder2(
              dictionary=task.source_dictionary,
-             embed_dim=args.encoder_embed_dim,
-             embed_dict=encoder_embed_dict,
-             max_positions=args.max_source_positions,
              dropout=args.dropout,
              num_inputs=args.gcn_num_inputs,
              num_units=args.gcn_num_units,
@@ -108,18 +114,10 @@ class FConvGCNModel(FairseqGCNModel):
              batch_first=args.gcn_batch_first,
              residual=args.gcn_residual,
              use_gates=args.gcn_use_gates,
-             use_glus=args.gcn_use_glus
+             use_glus=args.gcn_use_glus,
+             normalization_constant=args.normalization_constant,
         )
-        conv_encoder = FConvEncoder2(
-            dictionary=task.source_dictionary,
-            embed_dim=args.encoder_embed_dim,
-            embed_dict=encoder_embed_dict,
-            convolutions=eval(args.encoder_layers),
-            dropout=args.dropout,
-            max_positions=args.max_source_positions,
-            normalization_constant=args.normalization_constant,
-        )
-        encoder = FConvGCNEncoder(task.source_dictionary, gcn_encoder, conv_encoder)
+        encoder = FConvGCNOnTopEncoder(task.source_dictionary, conv_encoder, gcn_encoder)
         decoder = FConvDecoder(
             dictionary=task.target_dictionary,
             embed_dim=args.decoder_embed_dim,
@@ -132,10 +130,10 @@ class FConvGCNModel(FairseqGCNModel):
             share_embed=args.share_input_output_embed,
             normalization_constant=args.normalization_constant,
         )
-        return FConvGCNModel(encoder, decoder)
+        return FConvGCNOnTopModel(encoder, decoder)
 
 
-class FConvEncoder2(FairseqEncoder):
+class FConvEncoder3(FairseqEncoder):
     """Convolutional encoder"""
 
     def __init__(
@@ -149,20 +147,20 @@ class FConvEncoder2(FairseqEncoder):
         self.left_pad = left_pad
         self.num_attention_layers = None
 
-        # num_embeddings = len(dictionary)
+        num_embeddings = len(dictionary)
         self.padding_idx = dictionary.pad()
 
-        # self.embed_tokens = Embedding(num_embeddings, embed_dim, self.padding_idx)
-        # if embed_dict:
-        #     self.embed_tokens = utils.load_embedding(embed_dict, self.dictionary, self.embed_tokens)
-        #
-        # self.embed_positions = PositionalEmbedding(
-        #     max_positions,
-        #     embed_dim,
-        #     self.padding_idx,
-        #     left_pad=self.left_pad
-        #     # left_pad=False, #TODO: check LearnedPositionalEmbedding.forward() for the case of True
-        # )
+        self.embed_tokens = Embedding(num_embeddings, embed_dim, self.padding_idx)
+        if embed_dict:
+            self.embed_tokens = utils.load_embedding(embed_dict, self.dictionary, self.embed_tokens)
+
+        self.embed_positions = PositionalEmbedding(
+            max_positions,
+            embed_dim,
+            self.padding_idx,
+            left_pad=self.left_pad
+            # left_pad=False, #TODO: check LearnedPositionalEmbedding.forward() for the case of True
+        )
 
         convolutions = extend_conv_spec(convolutions)
         in_channels = convolutions[0][0]
@@ -192,14 +190,27 @@ class FConvEncoder2(FairseqEncoder):
             layer_in_channels.append(out_channels)
         self.fc2 = Linear(in_channels, embed_dim)
 
-    def forward(self, memory_bank, src, embeddings):
-        input_embedding = embeddings
+    def forward(self, src_tokens, src_lengths):
+        torch.set_printoptions(threshold=8000)
+
+        x1 = self.embed_tokens(src_tokens)
+        x2 = self.embed_positions(src_tokens)
+
+        if src_tokens.lt(0).sum() > 0:
+            print("negative voc idx (voc size {})".format(len(self.dictionary)))
+            print(src_tokens)
+            exit()
+
+        x = x1 + x2
+
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        input_embedding = x
 
         # project to size of convolution
-        x = self.fc1(memory_bank + embeddings)
+        x = self.fc1(x)
 
         # used to mask padding in input
-        encoder_padding_mask = src.eq(self.padding_idx).t()  # -> T x B  ###puts 1's where the pad index 0's otherwise
+        encoder_padding_mask = src_tokens.eq(self.padding_idx).t()  # -> T x B  ###puts 1's where the pad index 0's otherwise
         if not encoder_padding_mask.any():
             encoder_padding_mask = None
 
@@ -251,15 +262,19 @@ class FConvEncoder2(FairseqEncoder):
         y = (x + input_embedding) * math.sqrt(self.normalization_constant)
 
         return {
-            'encoder_out': (x, y),
+            'encoder_out': (input_embedding, y),           # modified here
             'encoder_padding_mask': encoder_padding_mask,  # B x T
         }
 
+    def max_positions(self):
+        """Maximum input length supported by the encoder."""
+        return self.embed_positions.max_positions()
 
-class GCNEncoder(FairseqEncoder):
+
+class GCNEncoder2(FairseqEncoder):
     """ A graph convolutional encoder"""
 
-    def __init__(self, dictionary, embed_dim, embed_dict, max_positions, dropout,
+    def __init__(self, dictionary, dropout,
                  num_inputs, num_units, num_labels, num_layers=1,
                  in_arcs=True,
                  out_arcs=True,
@@ -268,24 +283,26 @@ class GCNEncoder(FairseqEncoder):
                  use_gates=True,
                  use_glus=False,
                  # morph_embeddings=None,
-                 left_pad=True):
-        super(GCNEncoder, self).__init__(dictionary)
-        num_embeddings = len(dictionary)
+                 left_pad=True,
+                 normalization_constant=5):
+        super(GCNEncoder2, self).__init__(dictionary)
+        # num_embeddings = len(dictionary)
         self.padding_idx = dictionary.pad()
         self.left_pad = left_pad
         self.dropout = dropout
         self.batch_first = batch_first
-        self.embed_tokens = Embedding(num_embeddings, embed_dim, self.padding_idx)
-        if embed_dict:
-            self.embed_tokens = utils.load_embedding(embed_dict, self.dictionary, self.embed_tokens)
-
-        self.embed_positions = PositionalEmbedding(
-            max_positions,
-            embed_dim,
-            self.padding_idx,
-            left_pad=self.left_pad
-            # left_pad=False, #TODO: check LearnedPositionalEmbedding.forward() for the case of True
-        )
+        self.normalization_constant = normalization_constant
+        # self.embed_tokens = Embedding(num_embeddings, embed_dim, self.padding_idx)
+        # if embed_dict:
+        #     self.embed_tokens = utils.load_embedding(embed_dict, self.dictionary, self.embed_tokens)
+        #
+        # self.embed_positions = PositionalEmbedding(
+        #     max_positions,
+        #     embed_dim,
+        #     self.padding_idx,
+        #     left_pad=self.left_pad
+        #     # left_pad=False, #TODO: check LearnedPositionalEmbedding.forward() for the case of True
+        # )
 
         self.num_layers = num_layers
         self.num_inputs = num_inputs
@@ -334,23 +351,14 @@ class GCNEncoder(FairseqEncoder):
             self.gcn_seq = nn.Sequential(*self.gcn_layers)
 
 
-    def forward(self, src, lengths=None, arc_tensor_in=None, arc_tensor_out=None,
+    def forward(self, conv_output, lengths=None, arc_tensor_in=None, arc_tensor_out=None,
                 label_tensor_in=None, label_tensor_out=None,
                 mask_in=None, mask_out=None,  # batch* t, degree
                 mask_loop=None, sent_mask=None):
 
-        torch.set_printoptions(threshold=8000)
-
-        x1 = self.embed_tokens(src)
-        x2 = self.embed_positions(src)
-        if src.lt(0).sum() > 0:
-            print("negative voc idx (voc size {})".format(len(self.dictionary)))
-            print(src)
-            exit()
-
-        x = x1 + x2
-
-        embeddings = F.dropout(x, p=self.dropout, training=self.training)
+        input_embedding, y = conv_output['encoder_out']
+        encoder_padding_mask = conv_output['encoder_padding_mask']
+        y = F.dropout(y, p=self.dropout, training=self.training)
         # if morph is None:
 
         # else:
@@ -373,7 +381,7 @@ class GCNEncoder(FairseqEncoder):
 
             for g, gcn in enumerate(self.gcn_layers):
                 if g == 0:
-                    memory_bank = gcn(embeddings, lengths, arc_tensor_in, arc_tensor_out,
+                    memory_bank = gcn(y, lengths, arc_tensor_in, arc_tensor_out,
                                                  label_tensor_in, label_tensor_out,
                                                  mask_in, mask_out,
                                                  mask_loop, sent_mask)  # [t, b, h]
@@ -394,7 +402,7 @@ class GCNEncoder(FairseqEncoder):
 
             for g, gcn in enumerate(self.gcn_layers):
                 if g == 0:
-                    memory_bank = gcn(embeddings, lengths, arc_tensor_in, arc_tensor_out,
+                    memory_bank = gcn(y, lengths, arc_tensor_in, arc_tensor_out,
                                                  label_tensor_in, label_tensor_out,
                                                  mask_in, mask_out,
                                                  mask_loop, sent_mask)  # [t, b, h]
@@ -402,7 +410,7 @@ class GCNEncoder(FairseqEncoder):
                     memory_bank = memory_bank.transpose(0, 1)  # [b, t, h]
 
                 elif g == 1:
-                    prev_memory_bank = embeddings+memory_bank
+                    prev_memory_bank = y + memory_bank
                     memory_bank = gcn(prev_memory_bank, lengths, arc_tensor_in, arc_tensor_out,
                                       label_tensor_in, label_tensor_out,
                                       mask_in, mask_out,
@@ -424,7 +432,7 @@ class GCNEncoder(FairseqEncoder):
         elif self.residual == 'dense':
             for g, gcn in enumerate(self.gcn_layers):
                 if g == 0:
-                    memory_bank = gcn(embeddings, lengths, arc_tensor_in, arc_tensor_out,
+                    memory_bank = gcn(y, lengths, arc_tensor_in, arc_tensor_out,
                                                  label_tensor_in, label_tensor_out,
                                                  mask_in, mask_out,
                                                  mask_loop, sent_mask)  # [t, b, h]
@@ -432,7 +440,7 @@ class GCNEncoder(FairseqEncoder):
                     memory_bank = memory_bank.transpose(0, 1)  # [b, t, h]
 
                 elif g == 1:
-                    prev_memory_bank = torch.cat([embeddings, memory_bank], dim=2)
+                    prev_memory_bank = torch.cat([y, memory_bank], dim=2)
                     memory_bank = gcn(prev_memory_bank, lengths, arc_tensor_in, arc_tensor_out,
                                       label_tensor_in, label_tensor_out,
                                       mask_in, mask_out,
@@ -466,18 +474,21 @@ class GCNEncoder(FairseqEncoder):
         # h__1 = torch.cat([h_1, h_2], dim=0)  # [2, b, h]
         # h__2 = torch.cat([h_3, h_4], dim=0)  # [2, b, h]
 
-        return memory_bank, embeddings
 
-    def max_positions(self):
-        """Maximum input length supported by the encoder."""
-        return self.embed_positions.max_positions()
+        # add output to input embedding for attention
+        y = (memory_bank + input_embedding) * math.sqrt(self.normalization_constant)
+
+        return {
+            'encoder_out': (memory_bank, y),
+            'encoder_padding_mask': encoder_padding_mask,  # B x T
+        }
 
 
-class FConvGCNEncoder(FairseqEncoder):
+class FConvGCNOnTopEncoder(FairseqEncoder):
     """ A convolutional-GCN encoder."""
 
-    def __init__(self, dictionary, gcn_encoder, conv_encoder):
-        super(FConvGCNEncoder, self).__init__(dictionary)
+    def __init__(self, dictionary, conv_encoder, gcn_encoder):
+        super(FConvGCNOnTopEncoder, self).__init__(dictionary)
         self.gcn_encoder = gcn_encoder
         self.conv_encoder = conv_encoder
 
@@ -485,9 +496,9 @@ class FConvGCNEncoder(FairseqEncoder):
                 label_tensor_in=None, label_tensor_out=None,
                 mask_in=None, mask_out=None,  # batch* t, degree
                 mask_loop=None, sent_mask=None, morph=None, morph_mask=None):
-        memory_bank, embeddings = self.gcn_encoder(src, lengths, arc_tensor_in, arc_tensor_out,
+        conv_output = self.conv_encoder(src, lengths)
+        encoder_output = self.gcn_encoder(conv_output, lengths, arc_tensor_in, arc_tensor_out,
                 label_tensor_in, label_tensor_out, mask_in, mask_out, mask_loop, sent_mask)
-        encoder_output = self.conv_encoder(memory_bank, src, embeddings)
 
         return encoder_output
 
@@ -504,10 +515,10 @@ class FConvGCNEncoder(FairseqEncoder):
 
     def max_positions(self):
         """Maximum input length supported by the encoder."""
-        return self.gcn_encoder.max_positions()
+        return self.conv_encoder.max_positions()
 
 
-@register_model_architecture('fconv_gcn', 'fconv_gcn')
+@register_model_architecture('fconv_gcn_on_top', 'fconv_gcn_on_top')
 def base_architecture(args):
     args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 512)
     args.encoder_embed_path = getattr(args, 'encoder_embed_path', None)
@@ -521,7 +532,7 @@ def base_architecture(args):
     args.normalization_constant = getattr(args, 'normalization_constant', 0.5)
 
 
-@register_model_architecture('fconv_gcn', 'fconv_gcn_wikicatsum')
+@register_model_architecture('fconv_gcn_on_top', 'fconv_gcn_on_top_wikicatsum')
 def fconv_gcn_wikicatsum(args):
     args.gcn_num_inputs = getattr(args, 'gcn_num_inputs', 256)
     args.gcn_num_units = getattr(args, 'gcn_num_units', 256)
